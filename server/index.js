4c91e7079087b3f449f2a25980fa371f783ad469
tapia1013 <manbearpig1013@gmail.com>
2021-11-09 20:18:03 -0800

    removed comments

--- server/index.js
+++ server/index.js
@@ -6,9 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
 
-//clientId      407936200583-uksolb9od56nv6n4qhpo3a2le9gmrpei.apps.googleusercontent.com
 
-//clientSecret  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
 
 passport.use(new GoogleStrategy());
 
