1898a55d3d82d420af4fcabc556d447d1ece60a1
tapia1013 <manbearpig1013@gmail.com>
2021-11-09 20:17:34 -0800

    installed passpaort and googlepassport20 and setup google oauth keys and modified gitignore to not include the keys file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-});
+//clientId      407936200583-uksolb9od56nv6n4qhpo3a2le9gmrpei.apps.googleusercontent.com
 
-const PORT = process.env.PORT || 5000
+//clientSecret  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
 
+passport.use(new GoogleStrategy());
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
