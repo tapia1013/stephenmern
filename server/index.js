7ae925eeb9bf60f34abe8ee198f1c23a01b7fb53
tapia1013 <manbearpig1013@gmail.com>
2021-11-10 11:18:25 -0800

    clientid etc..

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 
 const app = express();
@@ -8,7 +9,15 @@ const app = express();
 
 
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
 
 
 const PORT = process.env.PORT || 5000;
